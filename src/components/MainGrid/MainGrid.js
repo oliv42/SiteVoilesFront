802cb1ac33ccf5cbaa7cc7c66fe067f94b3ca5ad
Olivier.r <olivier.reboul@gmail.com>
2023-01-25 19:50:51 +0100

    add the maker of the paraglider on the cards

--- src/components/MainGrid/MainGrid.js
+++ src/components/MainGrid/MainGrid.js
@@ -1,27 +1,33 @@
-import { useEffect, useState } from 'react';
-import { requestParagliderList } from '../../requests/paraglider'
-import Card from '../card/Card';
+import { useEffect, useState } from "react";
+import { requestParagliderList } from "../../requests/paraglider";
+import Card from "../card/Card";
 
 function MainGrid() {
-
-
     const fetchParaliderList = async function () {
         const paragliderList = await requestParagliderList();
         return paragliderList;
-    }
+    };
 
     const [paragliderList, setParagliderList] = useState([]);
 
     useEffect(() => {
-        fetchParaliderList().then(res => setParagliderList(res.data))
-    }, [])
-    
+        fetchParaliderList().then((res) => setParagliderList(res.data));
+    }, []);
 
     return (
-        <div className='flex flex-wrap gap-5'>
+        <div className="flex flex-wrap gap-5">
             {paragliderList.map((paraglider, key) => {
-                const  {id, name, type, release_year} = paraglider;
-               return <Card key={key} id={id} name={name} type={type} releaseYear={release_year}/>
+                const { id, name, type, release_year, maker_name } = paraglider;
+                return (
+                    <Card
+                        key={key}
+                        id={id}
+                        name={name}
+                        type={type}
+                        releaseYear={release_year}
+                        maker_name={maker_name}
+                    />
+                );
             })}
         </div>
     );
